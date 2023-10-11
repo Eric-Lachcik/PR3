61ab44c852fb4e3a4362ceac1646d823029b2145
Eric-Lachcik <lachcikeric@gmail.com>
2023-10-11 15:49:58 +0200

    Actividad2

---
+++ Act2/Act2.py
@@ -0,0 +1,9 @@
+# Algorimto Primero
+A =  int(input("A: "))
+B =  int(input("B: "))
+suma = A + B
+resta = A - B
+multi = A * B
+divi = A / B
+
+print("La suma es",suma ,"la resta es",resta,"La multiplicacion es",multi,"y la division es ",divi )
