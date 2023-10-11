47e8bb965aef1b6fe8915ddd8765dacaac9c9856
Eric-Lachcik <lachcikeric@gmail.com>
2023-10-11 17:28:47 +0200

    Acabado

---
+++ Act2/Act2.py
@@ -0,0 +1,10 @@
+A = int(input("Introduce A:"))
+B = int(input("Introduce B:"))
+
+div = A/B
+div1 = B/A
+if A > B:
+    print("La división entre A y B es",div)
+
+else:
+    print("La divsión entre B y A es",div1)
