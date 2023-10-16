6f4542c2f0a0db2b5665b9e9e03212a015d3958a
Eric-Lachcik <lachcikeric@gmail.com>
2023-10-16 18:11:12 +0200

    Ex1 hecho

---
+++ Act6/Ex1.py
@@ -0,0 +1,8 @@
+A = input("Introduzca tu nombre:")
+B = input("Introduzca tu apellido:")
+C = input("Introduzca tu curso:")
+D = input("Introduzca tu color favorito:")
+E = input("Introduzca tu pelicula favorita:")
+
+
+print("El alumno", A , B ,"estudia", C ,",su color favorito es el", D ,"y su pelicula favorita es", E)
