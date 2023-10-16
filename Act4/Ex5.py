cb7ccd8fc3a33beb0d7d2793bcee528bb42c1a12
Eric-Lachcik <lachcikeric@gmail.com>
2023-10-16 16:34:29 +0200

    Ex5 hecho

---
+++ Act4/Ex5.py
@@ -0,0 +1,11 @@
+A = int(input("A:"))
+B = int(input("B:"))
+C = int(input("C:"))
+
+pro = A*B*C
+suma = A+B+C
+
+if A < 0:
+    print("El producto es",pro )
+else:
+    print("La suma total es", suma)
