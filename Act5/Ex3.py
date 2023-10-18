e9a6c411f13a24e2090ba32bba3089e2cbd8b3a1
Eric-Lachcik <lachcikeric@gmail.com>
2023-10-18 15:34:13 +0200

    Problema arreglado

--- Act5/Ex3.py
+++ Act5/Ex3.py
@@ -2,16 +2,13 @@ A = int(input("Introduce el dia:"))
 B = int(input("Introduce el mes:"))
 
 if 1 <= A <= 31:
-        if A > 0: 
+         
             print("Dia correcto")
-        else:
-         print("Dia incorrecto")
-        if 1 <=  B <= 12:
-            if B > 0:
-                 print("Mes correcto")
-                 print("La es fecha introducida es ",A , "del",B ,"del 2023")
-            else:
-                 print("Mes incorrecto")
-        else:("Mes incorrecto")
 else:
-     print("Dia incorrecto")
+         print("Dia incorrecto")
+if 1 <=  B <= 12:           
+     print("Mes correcto")     
+else:
+     print("Mes incorrecto")  
+if 1 <= A <= 31 and 1 <=  B <= 12:
+        print("La fecha introducida es el ", A , "de", B ,"del 2023")
