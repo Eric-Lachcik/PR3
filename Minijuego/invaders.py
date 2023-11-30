55d3084f8274233f74712ce1d5642f783cc55e5d
Eric-Lachcik <lachcikeric@gmail.com>
2023-11-30 17:21:24 +0100

    Fase intermedia

--- Minijuego/invaders.py
+++ Minijuego/invaders.py
@@ -1,9 +1,17 @@
 import pygame
 
 pygame.init()
-pantalla = pygame.display.set.mode((800,800))
+pantalla = pygame.display.set_mode((800,800))
+
+imagen_avion = pygame.image.load("cosa.png")
+avion = pygame.transform.scale(imagen_avion,(90,150))
+avion_rect = avion.get_rect()
+
+
+
 
 salir = False
+
 
 posIzda = 30
 posTop = 30
@@ -14,19 +22,20 @@ while not salir:
         if event.type == pygame.QUIT:
             salir = True
     
-    teclas = pygame.key.get.pressed()
-    if teclas(pygame.K_LEFT):
-        posIzda = 1
-    if teclas(pygame.K_RIGHT):
-        posIzda = 1
-    if teclas(pygame.K_TOP):
-        posTop = 1
-    if teclas(pygame.K_DOWN):
-        posTop = 1
+    teclas = pygame.key.get_pressed()
+    if teclas[pygame.K_LEFT]:
+        posIzda -= 1
+    if teclas[pygame.K_RIGHT]:
+        posIzda += 1
+    if teclas[pygame.K_UP]:
+        posTop -= 1
+    if teclas[pygame.K_DOWN]:
+        posTop += 1
     # gestionar cambios
     pantalla.fill((80,80,80))
 
-    pygame.draw.rect(pantalla , (80,80,80), pygame.Rect(posIzda,posTop,68,68))
+    pantalla.blit(avion, (posIzda,posTop))
+    #pygame.draw.rect(pantalla , (255,255,255), pygame.Rect(posIzda,posTop,68,68))
 
     # redibujar juego
     pygame.display.flip()
