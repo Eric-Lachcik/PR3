8366fdd9153af66d315776edccf296e32d1103ea
Eric-Lachcik <lachcikeric@gmail.com>
2024-01-18 17:04:02 +0100

    Bases

---
+++ Juegoprincipal/elements.py
@@ -0,0 +1,25 @@
+from typing import Any
+import pygame
+from pygame.sprite import  Group
+
+
+class Planeta(pygame.sprite.Sprite):
+    #Constructor de la Nave
+    def __init__(self, posicion):
+        super().__init__()
+        #cargamos las imagenes
+        self.ricardo = pygame.image.load("PlanetaF1.png")
+        self.ricardo2 = pygame.transform.scale(self.ricardo, (90,90))
+        self.image = self.ricardo2
+        self.mask = pygame.mask.from_surface(self.image)
+        #Rectangulo para la imagen
+        self.rect = self.image.get_rect()
+        #Actualizamos el rectangulo para que coincida con la imagen
+        self.rect.topleft = posicion
+
+class Fondo(pygma.sprite.Sprite):
+    
+    def __init__(self,) -> None:
+        super().__init__()
+        #Cargamos la imagen del Fondo
+        
---
+++ Juegoprincipal/main.py
@@ -0,0 +1,50 @@
+import pygame
+import elements
+
+
+#Iniciamos el Juego
+pygame.init()
+
+
+#Tamaño de la Pantalla y posicion de la Nave
+tamanio = (800, 800)
+pantalla = pygame.display.set_mode(tamanio)
+posicion = (360,350)
+#Reloj del juego y FPS
+reloj = pygame.time.Clock()
+FPS = 60
+
+#Fuente para el texto(Menu)
+font = pygame.font.Font(None, 30)
+
+#Booleano de Control
+running = [True]
+
+#Grupo de Sprites
+grupo_sprites_todos = pygame.sprite.Group()
+grupo_sprites_enemigos = pygame.sprite.Group()
+grupo_sprites_balas = pygame.sprite.Group()
+
+grupo_sprites_todos.add(elements.Planeta(posicion))
+
+#Bucle Principal
+while running[0]:
+    #Limitamos los FrameRate
+    reloj.tick(FPS)
+    
+    #Gestionamos la Salida
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running[0] = False
+
+    #Capturamos la Teclas
+    teclas = pygame.key.get_pressed()
+    
+    
+    #Pintamos la Pantalla
+    pantalla.fill((80,80,80))
+    grupo_sprites_todos.draw(pantalla)
+    grupo_sprites_todos.update(teclas, grupo_sprites_todos, grupo_sprites_balas, grupo_sprites_enemigos, running)
+    #Rdibujamos la Pantalla
+    pygame.display.flip()
+    pass
